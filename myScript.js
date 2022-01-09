ddb6b04e8f91a27af70d4cfe4519c09f174719f3
MoistButteryBiscuits <Mikeleblanc8489@gmail.com>
2022-01-09 14:03:33 -0500

    Create function to generate 16x16 grid

--- myScript.js
+++ myScript.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+//global variables
+const DEFAULT_SIZE = 16;
+const grid = document.getElementById("grid")
+
+//generates grid based of size
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 1; i <= size * size; i++) {
+        const div = document.createElement("div");
+        div.className = ("grid-item");
+        grid.appendChild(div);
+    }
+}
+
+setupGrid(DEFAULT_SIZE);
