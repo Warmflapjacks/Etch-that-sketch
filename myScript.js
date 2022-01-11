c28c4190772f1ad09531259ceb5eae24bd2f3da1
MoistButteryBiscuits <Mikeleblanc8489@gmail.com>
2022-01-10 19:14:30 -0500

    Dynamically create grid

--- myScript.js
+++ myScript.js
@@ -1,16 +1,27 @@
 //global variables
 const DEFAULT_SIZE = 16;
-const grid = document.getElementById("grid")
+const grid = document.getElementById("grid");
+const gridItem = document.getElementsByClassName("grid-item");
 
 //generates grid based of size
-function setupGrid(size) {
+function setupGrid (size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    //creates <div> with class name "grid-item"
     for (let i = 1; i <= size * size; i++) {
         const div = document.createElement("div");
         div.className = ("grid-item");
         grid.appendChild(div);
+
+        getBoxBackground(div);
+    }
+}
+
+//adds color to individual grids 
+function getBoxBackground (div) {
+    div.onmouseover = () => {
+        div.classList.add("color-box");
     }
 }
 
